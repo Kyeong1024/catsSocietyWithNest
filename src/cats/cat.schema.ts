25ccb79615271d67ba4da78df56ca6417deaf164
Kyeong1024 <aofldjxm@gmail.com>
2022-08-24 01:15:24 +0900

    Add: for only need data for client

--- src/cats/cat.schema.ts
+++ src/cats/cat.schema.ts
@@ -6,6 +6,8 @@ export type CatDocument = Cat & Document;
 
 @Schema({ timestamps: true })
 export class Cat {
+  id: string;
+
   @Prop({ required: true, unique: true })
   @IsEmail()
   @IsNotEmpty()
@@ -24,6 +26,16 @@ export class Cat {
   @Prop()
   @IsString()
   imageUrl: string;
+
+  readonly readOnlyData: { email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
