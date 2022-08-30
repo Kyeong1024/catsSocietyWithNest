b6d94d36044cf8eb6a6189e195daed8d9d96f7ac
Kyeong1024 <aofldjxm@gmail.com>
2022-08-31 00:10:34 +0900

    Add: protect access api docs

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // class validation을 사용하려면 필요
+
+  // swagger 문서 설정 아래에 해주면 동작 안함. 순서중요!
+  app.use(
+    '/api',
+    basicAuth({
+      challenge: true,
+      users: { [process.env.API_DOCS_ID]: process.env.API_DOCS_PW },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('C.I.C')
